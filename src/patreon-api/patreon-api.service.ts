e9a46557f9d20ad85a066d5f7f0328a8b4c1ce69
LavenderEdit <gercermagden@gmail.com>
2025-11-01 12:31:10 -0500

    feat: add patreon-api.ts dependency and update PatreonApiService to initialize OAuth client

--- src/patreon-api/patreon-api.service.ts
+++ src/patreon-api/patreon-api.service.ts
@@ -1,6 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-// import * as patreon from 'patreon';
+// import { Patreon } from 'patreon-api.ts';
 
 @Injectable()
-export class PatreonApiService {}
+export class PatreonApiService {
+    private patreonOAuthClient;
+
+    constructor(private configService: ConfigService) {
+        const clientId = this.configService.get<string>('PATREON_CLIENT_ID');
+        const clientSecret = this.configService.get<string>('PATREON_CLIENT_SECRET');
+        // this.patreonOAuthClient = Patreon.oauth(clientId, clientSecret);
+    }
+}
