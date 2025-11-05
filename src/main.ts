2097d3667b914022a25bbd8aa59167fa72ef59ae
LionelPastranaMK500 <andriypastrana1@gmail.com>
2025-11-05 15:08:41 -0500

    fix(main): update static file serving path to use process.cwd() for better compatibility

--- src/main.ts
+++ src/main.ts
@@ -18,23 +18,20 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const cookieSecret = configService.get<string>('COOKIE_SECRET');
 
-  // --- 4. Verificación de seguridad ---
   if (!cookieSecret) {
     throw new Error('COOKIE_SECRET no está definida en el archivo .env');
   }
 
-  // 5. Registramos el plugin CON EL SECRETO REAL
   await app.register(fastifyCookie, {
     secret: cookieSecret,
   });
 
   await app.register(fastifyStatic, {
-    root: join(__dirname, 'public'),
+    root: join(process.cwd(), 'dist', 'public'),
     prefix: '/',
   });
 
   const port = configService.get<number>('PORT') || 3000;
-
   await app.listen(port, '0.0.0.0');
 }
 bootstrap();
